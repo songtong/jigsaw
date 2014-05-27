72e352ad753bae8f85b205da2a923419c23eff30
songtong <tong.song@dianping.com>
2014-05-27 17:37:22 +0800

    Initial Commit

---
+++ jigsaw/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  jigsaw
+//
+//  Created by Song on 14-5-27.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jigsaw/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  jigsaw
+//
+//  Created by Song on 14-5-27.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
